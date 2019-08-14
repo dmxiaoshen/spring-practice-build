f07aaca306a40508c00fc84e986949b20b77c291
hushengen <hsg@dmxiaoshen.com>
2019-08-14 18:01:19 +0800

    modify

---
+++ apps/app-business/src/main/java/com/xyz/app/business/AppBusinessApplication.java
@@ -0,0 +1,30 @@
+package com.xyz.app.business;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.xyz.common.client.feign"})
+@EnableTransactionManagement
+public class AppBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBusinessApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ apps/app-business/src/main/java/com/xyz/app/business/aop/ControllerLogAspect.java
@@ -0,0 +1,19 @@
+package com.xyz.app.business.aop;
+
+import com.xyz.common.base.aop.AbstractControllerLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class ControllerLogAspect extends AbstractControllerLogAspect {
+
+    public static final String point = "execution(public * com.xyz.app.business.controller..*.*(..))";
+
+    @Pointcut(point)
+    @Override
+    public void log() {}
+}
